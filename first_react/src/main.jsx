08bb99be7202602c03e7718549e8f9cf06b3d80b
David2427 <da863s@login.missouristate.edu>
2025-11-29 02:50:58 -0600

    Edits with landing page and dashboard

--- first_react/src/main.jsx
+++ first_react/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-// Optional: Reset/Global styles
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
